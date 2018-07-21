b64847bce8851edc5df5a663a613f376d6c2a88d
sbx_user1061 <sbx_user1061@ip-10-12-69-12.eu-west-2.compute.internal>
2018-07-21 14:53:23 +0000

    Sat Jul 21 14:53:23 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -4,12 +4,27 @@ import befaster.runner.SolutionNotImplementedException;
 
 public class FizzBuzzSolution {
 
+    public boolean contains(int c, int number) {
+        
+        while (number) {
+            int a = number%10;
+            if (a == c) {
+                return true;
+            }
+            number = number/10;
+        }
+        return false;
+    }
+
     public String fizzBuzz(Integer number) {
-        if (number%3 == 0 && number%5 == 0) {
+        boolean containsThree = contains(3,number);
+        boolean containsFive = contains(5, number);
+        if ( (number%3 == 0 && number%5 == 0) || 
+            (containsThree && containsFive) ) {
             return "fizz buzz";
-        } else if (number%3 == 0) {
+        } else if (number%3 == 0 || containsThree) {
             return "fizz";
-        } else if (number%5 == 0) {
+        } else if (number%5 == 0 || containsFive) {
             return "buzz";
         } else {
             return number + "";
